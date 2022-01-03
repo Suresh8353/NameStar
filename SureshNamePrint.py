213a0700fe351e372644ecdd2f5afad285f28d4b
Suresh8353 <81924160+Suresh8353@users.noreply.github.com>
2022-01-03 13:39:36 +0530

    Add files via upload

---
+++ SureshNamePrint.py
@@ -0,0 +1,7 @@
+import letter as S
+S.S()
+S.U()
+S.R()
+S.E()
+S.S()
+S.H()
---
+++ letter.py
@@ -0,0 +1,481 @@
+def A():
+          print("--------------------------------A--------------------------------------")
+          for i in range(0,7):
+                  for j in range(0,24):
+                            if ((j>=7-i and j<=14-i )or (j>=10+i and j<=17+i)):
+                                      print("*",end="")
+                            else:
+                                      print(end=" ",)
+                  print()
+                    
+          for i in range(0,5):
+               for j in range(0,1):
+                    print("********         ********",end="")
+               print(end="\n")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,5):
+               for j in range(0,1):
+                    print("********         ********",end="")
+               print(end="\n")
+          print(end="\n")
+
+
+def B():
+          print("--------------------------------B-----------------------------------")
+          for i in range(0,2):
+               for j in range(0,23):
+                  print("*",end="")
+               print(end="\n")
+          for i in range(0,2):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,6):
+               for j in range(0,1):
+                    print("********          *******",end="")
+               print(end="\n")
+          for i in range(0,4):
+               for j in range(0,24):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,6):
+               for j in range(0,1):
+                    print("********          *******",end="")
+               print(end="\n")
+
+          for i in range(0,2):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,2):
+               for j in range(0,23):
+                    print("*",end="")
+               print(end="\n")
+          print(end="\n")
+def C():
+          print("--------------------------------C-----------------------------------")
+          print("     **************")
+          print("  ********************")
+          print(" **********************")
+          print("************************")
+          print("********         ********")
+          print("********         ********")
+          print("********         ********")
+          for i in range(1,12):
+                    for j in range(1,9):
+                              print("*",end="")
+                    print()
+          print("********         ********")
+          print("********         ********")
+          print("********         ********")
+          print(" ***********************")
+          print("  *********************")
+          print("   *******************")
+          print("     ***************")
+def D():
+        print("--------------------------------D-----------------------------------")
+        for i in range(0,2):
+             for j in range(0,22):
+                  print("*",end="")
+             print(end="\n")
+        for i in range(0,2):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+
+        for i in range(0,12):
+             for j in range(0,1):
+                  print("********         ********",end="")
+             print(end="\n")
+    
+        for i in range(0,2):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        for i in range(0,2):
+             for j in range(0,22):
+                  print("*",end="")
+             print(end="\n")
+        print(end="\n")
+def E():
+        print("------------------------------E-------------------------------------")
+        for i in range(0,4):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        for i in range(0,5):
+             for j in range(0,1):
+                  print("********",end="")
+             print(end="\n")
+        for i in range(0,4):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        for i in range(0,5):
+             for j in range(0,1):
+                  print("********",end="")
+             print(end="\n")
+        for i in range(0,4):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        print(end="\n")
+def F():
+        print("------------------------------F-------------------------------------")
+        for i in range(0,4):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        for i in range(0,7):
+             for j in range(0,1):
+                  print("********",end="")
+             print(end="\n")
+        for i in range(0,4):
+             for j in range(0,18):
+                  print("*",end="")
+             print(end="\n")
+        for i in range(0,7):
+             for j in range(0,1):
+                  print("********",end="")
+             print(end="\n")
+def G():
+        print("------------------------------G-------------------------------------")
+        for i in range(0,4):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        for i in range(0,8):
+             for j in range(0,1):  
+                    if i>=4:
+                            print("********    ******************",end="")
+                    else:
+                            print("********",end="")              
+             print(end="\n")
+        for i in range(1,4):
+                  for j in range(1,31):
+                            if(( j>=1 and j<=8) or (j>=13 and j<=19) or (j>=25 and j<=31)):
+                                      print("*",end="")
+                            else:
+                                      print(end=" ")          
+                  print()
+        for i in range(1,4):
+             for j in range(1,31):
+                  if ((j>=1 and j<=19) or (j>=25 and j<=31)):
+                            
+                             print("*",end="")
+                  else:
+                            print(end=" ")
+             print(end="\n")
+def H():
+        print("-------------------------------H------------------------------------")
+        for i in range(0,10):
+             for j in range(0,1):
+                  print("********         ********",end="")
+             print(end="\n")
+        for i in range(0,4):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        for i in range(0,10):
+             for j in range(0,1):
+                  print("********         ********",end="")
+             print(end="\n")
+        print(end="\n")
+def I():        
+          print("------------------------I------------------------------------------")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,15):
+                    for j in range(0,1):
+                              print("        ********",end="")
+                    print()
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+def J():
+          print("------------------------J------------------------------------------")
+          for i in range(0,4):
+               for j in range(0,30):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,10):
+                    for j in range(0,1):
+                              print("             ********",end="")
+                    print()
+          for i in range(0,14):
+               for j in range(0,22):
+                    if ((j>=i and j<=6+i) or (j>=13 and j<=20)):  
+                            print("*",end="")
+                    else:
+                              print(end=" ")
+               print(end="\n")
+def K():
+          print("---------------------------K--------------------------------------")
+          for i in range(0,13):
+               for j in range(0,30):
+                    if ((j>=0 and j<=7) or (j>=21-i and j<=28-i)):  
+                            print("*",end="")
+                    else:
+                              print(end=" ")
+               print(end="\n")
+          for i in range(0,13):
+               for j in range(0,30):
+                    if ((j>=0 and j<=7) or (j>=7+i and j<=15+i)):  
+                            print("*",end="")
+                    else:
+                              print(end=" ")
+               print(end="\n")
+def L():
+          print("---------------------------L--------------------------------------")
+          for i in range(0,15):
+                    for j in range(0,1):
+                              print("********",end="")
+                    print()
+          print("********           ******")
+          print("********           ******")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+def M():
+          print("-----------------------------M-----------------------------------")
+          for i in range(0,13):
+                    for j in range(0,35):
+                              if(((j>=1 and j<=8)or( j>=1+i and j<=8+i))or ((j>=27-i and j<=34-i)or(j>=27 and j<=35))):
+                                        print("*",end="")
+                              else:
+                                        print(" ",end="")
+                    print()
+          for i in range(1,6):
+                    for j in range(0,1):
+                              print(" ********                  ********",end="")
+                    print()
+def N():
+          print("-----------------------------N-----------------------------------")
+          for i in range(0,25):
+                    for j in range(0,35):
+                              if((j>=1 and j<=8)or( j>=1+i and j<=8+i)or(j>=27 and j<=35)):
+                                        print("*",end="")
+                              else:
+                                        print(" ",end="")
+                    print()
+def O():
+        print("--------------------------------O-----------------------------------")
+        print("  *********************")
+        print("  *********************")
+        for i in range(0,2):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+
+        for i in range(0,12):
+             for j in range(0,1):
+                  print("********         ********",end="")
+             print(end="\n")
+    
+        for i in range(0,2):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        print("  *********************")
+        print("  *********************")
+        
+        print(end="\n")
+def P():
+          print("------------------------------R-------------------------------------")
+          for i in range(0,4):
+              for j in range(0,25):
+                   print("*",end="")
+              print(end="\n")
+          for i in range(0,5):
+               for j in range(0,1):
+                    print("********         ********",end="")
+               print(end="\n")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,12):
+                    for j in range(0,8):
+                              print("*",end="")
+                    print()
+
+def Q():
+        print("--------------------------------O-----------------------------------")
+        print("  *********************")
+        print("  *********************")
+        for i in range(0,2):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+
+        for i in range(0,12):
+             for j in range(0,1):
+                  print("********         ********",end="")
+             print(end="\n")
+    
+        for i in range(0,2):
+             for j in range(0,25):
+                  print("*",end="")
+             print(end="\n")
+        print("  *********************")
+        print("  *********************")
+        for i in range(0,5):
+                  for j in range(0,35):
+                            if (j>=16+i and j<=22+i):
+                                      print("*",end="")
+                            else:
+                                      print(" ",end="")
+                  print()
+                            
+        
+        print(end="\n")
+def R():
+          print("------------------------------R-------------------------------------")
+          for i in range(0,4):
+              for j in range(0,25):
+                   print("*",end="")
+              print(end="\n")
+          for i in range(0,5):
+               for j in range(0,1):
+                    print("********         ********",end="")
+               print(end="\n")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+
+          for i in range(0,13):
+                    for j in range(0,30):
+                              if ((j>=0 and j<=8) or (j>=9+i and j<=17+i)):
+                                        print("*",end="")
+                              else:
+                                        print(end=" ")
+                    print()
+
+                   
+def S():
+          print("------------------------S------------------------------------------")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,5):
+               for j in range(0,1):
+                    print("********",end="")
+               print(end="\n")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,5):
+               for j in range(0,1):
+                    print("                 ********",end="")
+               print(end="\n")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          print(end="\n\n")
+def T():        
+          print("------------------------T------------------------------------------")
+          for i in range(0,4):
+               for j in range(0,30):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,19):
+                    for j in range(0,1):
+                              print("          ********",end="")
+                    print()
+
+def U():
+          print("-----------------------------U--------------------------------------")
+          for i in range(0,14):
+               for j in range(0,1):
+                    print("********         ********",end="")
+               print(end="\n")
+          print("********         ********")
+          print(" ********       ******** ")
+          print("  *********************  ")
+          print("   *****************     ")
+          print("      ************       ")    
+          print(end="\n")
+def V():
+        print("--------------------------------V-----------------------------------")
+        for i in range(0,12):
+             for j in range(0,1):
+                  print("********         ********",end="")
+             print(end="\n")
+        for i in range(0,7):
+                  for j in range(0,24):
+                            if ((j>=i and j<=8+i )or (j>=17-i and j<=24-i)):
+                                      print("*",end="")
+                            else:
+                                      print(end=" ")
+                  print()
+
+        print(end="\n")
+def W():
+          print("-----------------------------W-----------------------------------")
+          for i in range(1,6):
+                    for j in range(0,1):
+                              print(" ********                  ********",end="")
+                    print()
+
+          for i in range(0,13):
+                    for j in range(0,35):
+                              if(((j>=1 and j<=8)or( j>=14-i and j<=21-i))or ((j>=14+i and j<=21+i)or(j>=27 and j<=35))):
+                                        print("*",end="")
+                              else:
+                                        print(" ",end="")
+                    print()
+def X():
+          print("----------------------------------X----------------------------------")
+          for i in range(0,25):
+                    for j in range(0,35):
+                              if ((j>=i and j<=8+i)or (j>=25-i and j<=33-i)):
+                                        print("*",end="")
+                              else:
+                                        print(end=" ")
+                    print()
+
+def Y(): 
+         print("-------------------------------Y------------------------------------")
+         for i in range(1,19):
+              for j in range(1,30):
+                    if ((j>=i and j<=7+i) or (j>=19 and j<=26)):
+                              print("*",end="")
+                    else:
+                              print(end=" ")
+              print()
+         for i in range(1,8):
+             for j in range(1,2):
+                    print("                  ********",end="")
+             print()
+
+         print(end="\n")
+def Z():
+          print("-------------------------------Z-------------------------------------")
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+          for i in range(0,15):
+                    for j in range(0,25):
+                              if (j>=15-i and j<=23-i):
+                                        print("*",end="")
+                              else:
+                                        print(end=" ")
+                    print()
+          for i in range(0,4):
+               for j in range(0,25):
+                    print("*",end="")
+               print(end="\n")
+
+
